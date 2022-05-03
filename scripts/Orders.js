cbba6e6c7a761e6bbdcc90240c04988563396a12
Griffin Moyer <moyerga@outlook.com>
2022-05-03 13:14:53 -0500

    added event listeners that change temporary and permanent state of database.

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,10 +1,64 @@
 import { getOrders } from "./database.js"
+import { getMetals } from "./database.js"
+import { getSizes } from "./database.js"
+import { getStyles } from "./database.js"
+
+const metals = getMetals()
+const sizes = getSizes()
+const styles = getStyles()
+
+const findMetalPrice = (order) => {
+    const foundMetal = metals.find(
+        (metal) => {
+            return metal.id === order.metalId
+        }
+    )
+    return foundMetal.price
+}
+
+const findSizePrice = (order) => {
+    const foundSize = sizes.find(
+        (size) => {
+            return size.id === order.sizeId
+        }
+    )
+    return foundSize.price
+}
+
+const findStylePrice = (order) => {
+    const foundStyle = styles.find(
+        (style) => {
+            return style.id === order.styleId
+        }
+    )
+    return foundStyle.price
+}
+
+const calcTotalPrice = (order) => {
+    let metalPrice = findMetalPrice(order)
+    let sizePrice = findSizePrice(order)
+    let stylePrice = findStylePrice(order)
+    const totalPrice = metalPrice + sizePrice + stylePrice
+    return totalPrice
+}
 
 const buildOrderListItem = (order) => {
+    
+    const totalPrice = calcTotalPrice(order)
+    const costString = totalPrice.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    })
+
     return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
+        Order #${order.id} was placed on ${order.timestamp}. Cost: ${costString}
     </li>`
 }
+
+
+
+// Remember that the function you pass to find() must return true/false
+
 
 export const Orders = () => {
     /*
